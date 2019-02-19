0356a80e483554c48d30dff6a8649e2184cdeac1
Oliver Seibert <SeibertO@torpedo-gruppe.de>
2019-02-19 20:27:51 +0100

    load only the leagues we want to + load only teams and fixtures for active leagues

--- api/lib/controllers/cronController.ts
+++ api/lib/controllers/cronController.ts
@@ -11,13 +11,22 @@ export class CronController {
 
   async getLeagues() {
     try {
-      const response = await fetch('https://oliver-seibert.de/tippkoenig/leagues.json');
-      const json = await response.json();
-      const leagues = _.toArray(_.get(json, 'api.leagues'));
-      console.log('leagues', _.size(leagues));
-      _.forEach(leagues, (league) => {
-        _.set(league, '_id', league.league_id);
-        this.leagueController.save(league);
+      const leaguesToLoad = ["8"]; // TODO: load from .env
+
+      // load only the leagues we want to
+      _.forEach(leaguesToLoad, async (leagueId) => {
+        const response = await fetch('https://oliver-seibert.de/tippkoenig/leagues.json');
+        // const response = await fetch(`https://api-football-v1.p.rapidapi.com/leagues/league/${leagueId}`);
+        const json = await response.json();
+        const leagues = _.toArray(_.get(json, 'api.leagues'));
+
+        // should only contain one item when loaded from api
+        console.log('leagues', _.size(leagues));
+
+        _.forEach(leagues, (league) => {
+          _.set(league, '_id', league.league_id);
+          this.leagueController.save(league);
+        });
       });
     } catch (error) {
       console.log(error);
@@ -26,13 +35,21 @@ export class CronController {
 
   async getTeams() {
     try {
-      const response = await fetch('https://oliver-seibert.de/tippkoenig/bundesliga-teams.json');
-      const json = await response.json();
-      const teams = _.toArray(_.get(json, 'api.teams'));
-      console.log('teams', _.size(teams));
-      _.forEach(teams, (team) => {
-        _.set(team, '_id', team.team_id);
-        this.teamController.save(team);
+      const activeLeagues = await this.leagueController.getActiveLeagues();
+
+      // iterate over activeLeagues and get teams for each
+      _.forEach(activeLeagues, async (league) => {
+        const response = await fetch('https://oliver-seibert.de/tippkoenig/bundesliga-teams.json');
+        // const response = await fetch(`https://api-football-v1.p.rapidapi.com/teams/league/${league._id}`);
+        const json = await response.json();
+        const teams = _.toArray(_.get(json, 'api.teams'));
+
+        console.log(`teams count for league ${league.name}`, _.size(teams));
+
+        _.forEach(teams, (team) => {
+          _.set(team, '_id', team.team_id);
+          this.teamController.save(team);
+        });
       });
     } catch (error) {
       console.log(error);
@@ -42,16 +59,22 @@ export class CronController {
   async getFixtures() {
     try {
       const activeLeagues = await this.leagueController.getActiveLeagues();
-      // TODO: iterate over activeLeagues and get Fixtures
 
-      const response = await fetch('https://oliver-seibert.de/tippkoenig/bundesliga-fixtures.json');
-      const json = await response.json();
-      const fixtures = _.toArray(_.get(json, 'api.fixtures'));
-      console.log('fixtures', _.size(fixtures));
-      _.forEach(fixtures, (fixture) => {
-        _.set(fixture, '_id', fixture.fixture_id);
-        // TODO: parse matchday from attribute round
-        this.fixtureController.save(fixture);
+      // iterate over activeLeagues and get fixtures for each
+      _.forEach(activeLeagues, async (league) => {
+        const response = await fetch('https://oliver-seibert.de/tippkoenig/bundesliga-fixtures.json');
+        // const response = await fetch(`https://api-football-v1.p.mashape.com/fixtures/league/${league._id}`);
+        const json = await response.json();
+        const fixtures = _.toArray(_.get(json, 'api.fixtures'));
+
+        console.log(`fixtures count for league ${league.name}`, _.size(fixtures));
+
+        _.forEach(fixtures, (fixture) => {
+          _.set(fixture, '_id', fixture.fixture_id);
+          // parse matchday from attribute: e.g. get "11" from "Bundesliga - 11"
+          fixture.matchday = _.trim(_.last(_.split(fixture.round, '-')));
+          this.fixtureController.save(fixture);
+        });
       });
     } catch (error) {
       console.log(error);
